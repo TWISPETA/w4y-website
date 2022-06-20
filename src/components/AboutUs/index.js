56d46b5bc4e95f415f6ce9b759aabcb108c7f7cc
TWISPETA <lsmelo97@gmail.com>
2022-06-20 09:22:13 -0300

    base concluida

---
+++ src/components/AboutUs/index.js
@@ -0,0 +1,129 @@
+import { Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import GithubIcon from '../../assets/icons/github-icon.svg';
+import LinkedinIcon from '../../assets/icons/linkedin-icon.svg';
+import "./styles.css";
+
+
+
+export default function AboutUs() {
+
+    return (
+        <div id="about-us">
+
+            <h1>About us</h1>
+
+            <Swiper
+                slidesPerView={window.screen.width < 800 ? 1 : 3}
+                spaceBetween={30}
+                slidesPerGroup={window.screen.width < 800 ? 1 : 3}
+                loop={true}
+                loopFillGroupWithBlank={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={window.screen.width < 800 ? false : true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="img-box">
+                        J
+                    </div>
+
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+
+                    <div className="about-us-links">
+                        <a href='https://www.linkedin.com/in/leonardo-melo-896a98124/'>
+                            <img src={LinkedinIcon} alt="linkedin icon" />
+                        </a>
+
+                        <a href='https://github.com/TWISPETA'>
+                            <img src={GithubIcon} alt="github icon" />
+                        </a>
+                    </div>
+                </SwiperSlide><SwiperSlide>
+                    <div className="img-box">
+                        J
+                    </div>
+
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+
+                    <div className="about-us-links">
+                        <a href='https://www.linkedin.com/in/leonardo-melo-896a98124/'>
+                            <img src={LinkedinIcon} alt="linkedin icon" />
+                        </a>
+
+                        <a href='https://github.com/TWISPETA'>
+                            <img src={GithubIcon} alt="github icon" />
+                        </a>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="img-box">
+                        J
+                    </div>
+
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+
+                    <div className="about-us-links">
+                        <a href='https://www.linkedin.com/in/leonardo-melo-896a98124/'>
+                            <img src={LinkedinIcon} alt="linkedin icon" />
+                        </a>
+
+                        <a href='https://github.com/TWISPETA'>
+                            <img src={GithubIcon} alt="github icon" />
+                        </a>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="img-box">
+                        J
+                    </div>
+
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+
+                    <div className="about-us-links">
+                        <a href='https://www.linkedin.com/in/leonardo-melo-896a98124/'>
+                            <img src={LinkedinIcon} alt="linkedin icon" />
+                        </a>
+
+                        <a href='https://github.com/TWISPETA'>
+                            <img src={GithubIcon} alt="github icon" />
+                        </a>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="img-box">
+                        J
+                    </div>
+
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </p>
+
+                    <div className="about-us-links">
+                        <a href='https://www.linkedin.com/in/leonardo-melo-896a98124/'>
+                            <img src={LinkedinIcon} alt="linkedin icon" />
+                        </a>
+
+                        <a href='https://github.com/TWISPETA'>
+                            <img src={GithubIcon} alt="github icon" />
+                        </a>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Main from './components/Main';
+import AboutUs from './components/AboutUs';
+import { BrowserRouter } from 'react-router-dom';
+import Projects from './components/Projects';
+import Contacts from './components/Contacts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Main />
+    <AboutUs />
+    <Projects/>
+    <Contacts />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
